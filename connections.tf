785c4495a38cda9e1b5cc48c50cddd42e7f28b5a
netstarz77 <netstarz77@gmail.com>
2019-01-26 22:46:45 -0500

    hello world

---
+++ connections.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  project = "mystical-ally-210222"
+  region = "us-west1"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "azurerm"  {
+  subscription_id = "0"
+  client_id = "1"
+  client_secret = "2"
+  tenant_id = "3"
+}
